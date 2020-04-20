e0c788ac5ea9066a7a6f85075199ee2c0780f7b0
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-04-20 11:48:17 +0530

    chore: add new collections
    1. CallExpression
    2. FunctionDeclaration
    3. ImportDeclaration

---
+++ src/collections/CallExpression.js
@@ -0,0 +1,91 @@
+const Collection = require('jscodeshift/src/Collection');
+const NodeCollection = require('jscodeshift/src/collections/Node');
+const once = require('jscodeshift/src/utils/once');
+const recast = require('recast');
+const j = require('jscodeshift');
+
+const { CallExpression } = recast.types.namedTypes;
+
+function memberExpressionFilter(name) {
+  const parts = name.split('.');
+  if (parts.length === 2) {
+    return {
+      object: { name: parts[0] },
+      property: { name: parts[1] },
+    };
+  }
+  const exceptLast = parts.slice(0, parts.length - 1);
+  return {
+    object: memberExpressionFilter(exceptLast.join('.')),
+    property: { name: parts[parts.length - 1] },
+  };
+}
+
+/**
+* @mixin
+*/
+const globalMethods = {
+  /**
+   * Finds all call expressions optionally filtered by name.
+   *
+   * @param {string} name
+   * @return {Collection}
+   */
+  findCallExpressions(name) {
+    const callees = name ? name.split('.') : [];
+    let filter;
+    if (callees.length === 1) {
+      filter = { callee: { name } };
+    } else if (callees.length > 1) {
+      filter = { callee: memberExpressionFilter(name) };
+    } else {
+      filter = null;
+    }
+    return this.find(CallExpression, filter);
+  },
+};
+
+/**
+* @mixin
+*/
+const transformMethods = {
+  /**
+   * Renames a call expression
+   *
+   * @param {string} newName
+   * @return {Collection}
+   */
+  renameTo(newName) {
+    const isMemberExpression = newName.includes('.');
+    return this.forEach((path) => {
+      const node = path.value;
+      if (isMemberExpression) {
+        const [object, property] = newName.split('.');
+        node.callee = j.memberExpression(j.identifier(object), j.identifier(property));
+      } else {
+        node.callee.name = newName;
+      }
+    });
+  },
+
+  addParam(param) {
+    return this.forEach((path) => {
+      path.value.arguments.push(j.identifier(param));
+    });
+  },
+
+  removeParam(param) {
+    return this.forEach((path) => {
+      const newParams = path.value.arguments.filter((p) => p.name !== param);
+      path.value.arguments = newParams;
+    });
+  },
+};
+
+function register() {
+  NodeCollection.register();
+  Collection.registerMethods(globalMethods);
+  Collection.registerMethods(transformMethods, CallExpression);
+}
+
+exports.register = once(register);
---
+++ src/collections/FunctionDeclaration.js
@@ -0,0 +1,62 @@
+const Collection = require('jscodeshift/src/Collection');
+const NodeCollection = require('jscodeshift/src/collections/Node');
+const once = require('jscodeshift/src/utils/once');
+const recast = require('recast');
+const j = require('jscodeshift');
+
+const { FunctionDeclaration } = recast.types.namedTypes;
+
+/**
+* @mixin
+*/
+const globalMethods = {
+  /**
+   * Finds all function declarations, optionally filtered by name.
+   *
+   * @param {string} name
+   * @return {Collection}
+   */
+  findFunctionDeclarations(name) {
+    const filter = name ? { id: { name } } : null;
+    return this.find(FunctionDeclaration, filter);
+  },
+};
+
+/**
+* @mixin
+*/
+const transformMethods = {
+  /**
+   * Renames a function declaration
+   *
+   * @param {string} newName
+   * @return {Collection}
+   */
+  renameTo(newName) {
+    return this.forEach((path) => {
+      const node = path.value;
+      node.id.name = newName;
+    });
+  },
+
+  addParam(param) {
+    return this.forEach((path) => {
+      path.value.params.push(j.identifier(param));
+    });
+  },
+
+  removeParam(param) {
+    return this.forEach((path) => {
+      const newParams = path.value.params.filter((p) => p.name !== param);
+      path.value.params = newParams; // eslint-disable-line
+    });
+  },
+};
+
+function register() {
+  NodeCollection.register();
+  Collection.registerMethods(globalMethods);
+  Collection.registerMethods(transformMethods, FunctionDeclaration);
+}
+
+exports.register = once(register);
